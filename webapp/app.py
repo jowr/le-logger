0a3f200add780ebb1e8afda8bb384eb1b061b583
Jorrit Wronski <jowr@ipu.dk>
2017-03-20 22:48:58 +0100

    Added Bokeh

--- webapp/app.py
+++ webapp/app.py
@@ -1,21 +1,58 @@
+from __future__ import print_function, division
+
 import os, sys
+
 if 'DYNO' in os.environ:
     HEROKU = True
 else:
     HEROKU = False
 
-
+# The main app
 from flask import Flask
 app = Flask(__name__)
 
-# Start with the views
+# All the views
 @app.route('/')
 def hello():
     return 'Hello World with Flask and JJ!'
+
+@apply.route('/pic')
+def pic():
+    from bokeh.plotting import figure, output_file, show, save
+    from bokeh.embed import file_html
+
+    # prepare some data
+    x = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+    y0 = [i**2 for i in x]
+    y1 = [10**i for i in x]
+    y2 = [10**(i**2) for i in x]
+
+    # output to static HTML file
+    #output_file("log_lines.html")
+
+    # create a new plot
+    p = figure(
+       tools="pan,box_zoom,reset,save",
+       y_axis_type="log", y_range=[0.001, 10**11], title="log axis example",
+       x_axis_label='sections', y_axis_label='particles'
+    )
+
+    # add some renderers
+    p.line(x, x, legend="y=x")
+    p.circle(x, x, legend="y=x", fill_color="white", size=8)
+    p.line(x, y0, legend="y=x^2", line_width=3)
+    p.line(x, y1, legend="y=10^x", line_color="red")
+    p.circle(x, y1, legend="y=10^x", fill_color="red", line_color="red", size=6)
+    p.line(x, y2, legend="y=10^x^2", line_color="orange", line_dash="4 4")
+
+    # show the results
+    #show(p)
+
+    return file_html(p, {})
 
 # ... add the main method for Heroku at the end
 if HEROKU:
     if __name__ == '__main__':
         # Bind to PORT if defined, otherwise default to 5000.
         port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+        app.run(host='0.0.0.0', port=port)
