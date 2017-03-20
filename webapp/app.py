ca8fb56b6715e39bed7234d9cd143f30e9cc58fc
Jorrit Wronski <jowr@ipu.dk>
2017-03-20 21:23:23 +0100

    Changed the app to match pythonanywhere

--- webapp/app.py
+++ webapp/app.py
@@ -5,9 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World with Flask!'
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+#    # Bind to PORT if defined, otherwise default to 5000.
+#    port = int(os.environ.get('PORT', 5000))
+#app.run(host='0.0.0.0', port=port)
+
+## Default pythonanywhere flask app
+## A very simple Flask Hello World app for you to get started with...
+#from flask import Flask
+#app = Flask(__name__)
+#@app.route('/')
+#def hello_world():
+#    return 'Hello from Flask!'
+#
