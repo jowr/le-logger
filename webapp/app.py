1920cd7431db55fe044fbd429013847310d7d952
Jorrit Wronski <jowr@ipu.dk>
2017-03-20 21:27:09 +0100

    There we go with the first compromise

--- webapp/app.py
+++ webapp/app.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 def hello():
     return 'Hello World with Flask!'
 
-#if __name__ == '__main__':
-#    # Bind to PORT if defined, otherwise default to 5000.
-#    port = int(os.environ.get('PORT', 5000))
-#app.run(host='0.0.0.0', port=port)
+if 'DYNO' in os.environ:
+    HEROKU = True
+else:
+    HEROKU = False
+ 
+if HEROKU:
+    if __name__ == '__main__':
+        # Bind to PORT if defined, otherwise default to 5000.
+        port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 ## Default pythonanywhere flask app
 ## A very simple Flask Hello World app for you to get started with...
