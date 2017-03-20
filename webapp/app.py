cce2cf2a958d1818a40abe2090d0dc25a5673f35
Jorrit Wronski <jowr@ipu.dk>
2017-03-20 22:03:08 +0100

    A little bit of documentation

--- webapp/app.py
+++ webapp/app.py
@@ -1,28 +1,21 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World with Flask!'
-
+import os, sys
 if 'DYNO' in os.environ:
     HEROKU = True
 else:
     HEROKU = False
- 
+
+
+from flask import Flask
+app = Flask(__name__)
+
+# Start with the views
+@app.route('/')
+def hello():
+    return 'Hello World with Flask!'
+
+# ... add the main method for Heroku at the end
 if HEROKU:
     if __name__ == '__main__':
         # Bind to PORT if defined, otherwise default to 5000.
         port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-## Default pythonanywhere flask app
-## A very simple Flask Hello World app for you to get started with...
-#from flask import Flask
-#app = Flask(__name__)
-#@app.route('/')
-#def hello_world():
-#    return 'Hello from Flask!'
-#
