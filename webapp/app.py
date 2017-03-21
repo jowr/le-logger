8741f79e85cf636b1ec1c361480a12acee376336
Jorrit Wronski <jowr@ipu.dk>
2017-03-21 16:54:22 +0100

    Debug

--- webapp/app.py
+++ webapp/app.py
@@ -177,15 +177,16 @@ def test_database():
 
         Base.metadata.create_all(engine)
 
-        DBSession = sessionmaker(bind=engine)
-        session = DBSession()
-
-        #testcases = [{"numbers": [25, 33, 42, 55], "name": "David"}, {"numbers": [11, 33, 7, 19 ], "name":     "Salazar"}, {"numbers": [32, 6, 20, 23 ], "name": "Belinda"}, {"numbers": [19, 20, 27, 8 ], "name": "Casey"},     {"numbers": [25, 31, 10, 40 ], "name": "Kathie"}, {"numbers": [25, 20, 40, 39 ], "name": "Dianne"},     {"numbers": [1, 20, 18, 38 ], "name": "Cortez"} ]
+        #DBSession = sessionmaker(bind=engine)
+        #session = DBSession()
         #
-        #for t in testcases:
-        #    session.add(TestUser(name=t['name'], numbers=t['numbers']))
-        #session.commit()
-        return session.info
+        ##testcases = [{"numbers": [25, 33, 42, 55], "name": "David"}, {"numbers": [11, 33, 7, 19 ], "name":     "Salazar"}, {"numbers": [32, 6, 20, 23 ], "name": "Belinda"}, {"numbers": [19, 20, 27, 8 ], "name": "Casey"},     {"numbers": [25, 31, 10, 40 ], "name": "Kathie"}, {"numbers": [25, 20, 40, 39 ], "name": "Dianne"},     {"numbers": [1, 20, 18, 38 ], "name": "Cortez"} ]
+        ##
+        ##for t in testcases:
+        ##    session.add(TestUser(name=t['name'], numbers=t['numbers']))
+        ##session.commit()
+        #return session.info
+        return str(engine)
     except Exception as e:
         return str(e)
 
