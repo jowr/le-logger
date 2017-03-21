46d1a59a5a316f2741b9d229c5e1b175f3112afb
Jorrit Wronski <jowr@ipu.dk>
2017-03-21 13:39:05 +0100

    Added simple test

--- webapp/app.py
+++ webapp/app.py
@@ -138,6 +138,24 @@ def polynomial():
     except Exception as e:
         return str(e)
 
+@app.route("/dbtest_simple")
+def test_database_simple():
+    import os
+    import psycopg2
+    import urlparse
+
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return os.environ["DATABASE_URL"]
+
 @app.route("/dbtest")
 def test_database():
     try:
